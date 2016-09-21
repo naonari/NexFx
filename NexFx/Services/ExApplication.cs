2471938ab83ba86e5875aad3ffbdd5caf941ee87
naonari <naonari.nakamura@ck2.so-net.ne.jp>
2016-09-21 14:53:41 +0900

    多重起動抑止機能追加

---
+++ NexFx/Services/ExApplication.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NexFx.Services
+{
+    public sealed class ExApplication
+    {
+        /// <summary>
+        /// 現在のスレッドで標準のアプリケーション メッセージ ループの実行を開始し、指定したフォームを表示します。
+        /// </summary>
+        /// <param name="mainForm">表示するフォームを表す System.Windows.Forms.Form。</param>
+        /// <param name="forbidMultiple">多重起動の抑止を表す値。</param>
+        public static void Run(Form mainForm, bool forbidMultiple)
+        {
+            // 多重起動の禁止するかを判定します。
+            if (forbidMultiple)
+            {
+                // Mutexをインスタンス化します。
+                using (var mutex = new Mutex(false, Assembly.GetCallingAssembly().GetName().Name))
+                {
+                    // 多重起動の判定を行います。
+                    if (mutex.WaitOne(0, false))
+                    {
+                        // アプリケーションを実行します。
+                        Application.Run(mainForm);
+                    }
+
+                    // 本メソッドが実行されるまでガベージコレクションの対象から除外します。
+                    GC.KeepAlive(mutex);
+
+                    // Mutexを閉じます。
+                    mutex.Close();
+                }
+            }
+            else
+            {
+                // アプリケーションを実行します。
+                Application.Run(mainForm);
+            }
+        }
+
+        /// <summary>
+        /// System.Windows.Forms.ApplicationContext を使用して、現在のスレッドで標準のアプリケーション メッセージ ループの実行を開始します。
+        /// </summary>
+        /// <param name="context">アプリケーションが実行される System.Windows.Forms.ApplicationContext。</param>
+        /// <param name="forbidMultiple">多重起動の抑止を表す値。</param>
+        public static void Run(ApplicationContext context, bool forbidMultiple)
+        {
+            // 多重起動の禁止するかを判定します。
+            if (forbidMultiple)
+            {
+                // Mutexをインスタンス化します。
+                using (var mutex = new Mutex(false, Assembly.GetCallingAssembly().GetName().Name))
+                {
+                    // 多重起動の判定を行います。
+                    if (mutex.WaitOne(0, false))
+                    {
+                        // アプリケーションを実行します。
+                        Application.Run(context);
+                    }
+
+                    // 本メソッドが実行されるまでガベージコレクションの対象から除外します。
+                    GC.KeepAlive(mutex);
+
+                    // Mutexを閉じます。
+                    mutex.Close();
+                }
+            }
+            else
+            {
+                // アプリケーションを実行します。
+                Application.Run();
+            }
+        }
+
+        /// <summary>
+        /// 現在のスレッドで標準のアプリケーション メッセージ ループの実行を、フォームなしで開始します。
+        /// </summary>
+        /// <param name="forbidMultiple">多重起動の抑止を表す値。</param>
+        public static void Run(bool forbidMultiple)
+        {
+            // 多重起動の禁止するかを判定します。
+            if (forbidMultiple)
+            {
+                // Mutexをインスタンス化します。
+                using (var mutex = new Mutex(false, Assembly.GetCallingAssembly().GetName().Name))
+                {
+                    // 多重起動の判定を行います。
+                    if (mutex.WaitOne(0, false))
+                    {
+                        // アプリケーションを実行します。
+                        Application.Run();
+                    }
+
+                    // 本メソッドが実行されるまでガベージコレクションの対象から除外します。
+                    GC.KeepAlive(mutex);
+
+                    // Mutexを閉じます。
+                    mutex.Close();
+                }
+            }
+            else
+            {
+                // アプリケーションを実行します。
+                Application.Run();
+            }
+        }
+    }
+}
--- NexFxTest/Program.cs
+++ NexFxTest/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NexFx.Services;
 
 namespace NexFxTest
 {
@@ -16,7 +17,7 @@ namespace NexFxTest
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new TestForm());
+            ExApplication.Run(new TestForm(), true);
         }
     }
 }
